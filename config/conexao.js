b5aab3108f99a4bd77fa777b7e20d7a5cde67b55
Ana Julia <anajuliasilva3304@gmail.com>
2024-11-27 19:32:23 -0300

    update

--- config/conexao.js
+++ config/conexao.js
@@ -1,5 +1,5 @@
 import mongoose from 'mongoose';
 
-const conexao = await mongoose.connect('mongodb://localhost:27017/test')
+const conexao = await mongoose.connect('mongodb://localhost:3000/test')
 
 export default conexao
